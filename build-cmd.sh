c5ff84fb5e9d67ff464dff680e4c4b5982313da0
Nat Goodspeed <nat@lindenlab.com>
2016-04-07 11:43:59 -0400

    DRTVWR-418: Tweak for 64-bit builds and build-variables.

--- build-cmd.sh
+++ build-cmd.sh
@@ -6,31 +6,40 @@ cd "$(dirname "$0")"
 set -x
 # make errors fatal
 set -e
+# complain about unset env variables
+set -u
 
 if [ -z "$AUTOBUILD" ] ; then 
     fail
 fi
 
 if [ "$OSTYPE" = "cygwin" ] ; then
-    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+    autobuild="$(cygpath -u $AUTOBUILD)"
+else
+    autobuild="$AUTOBUILD"
 fi
 
 FONTCONFIG_SOURCE_DIR="fontconfig"
 
 
 # load autobuild provided shell functions and variables
-eval "$("$AUTOBUILD" source_environment)"
+set +x
+eval "$("$autobuild" source_environment)"
+set -x
+
+# set LL_BUILD and friends
+set_build_variables convenience Release
 
 stage="$(pwd)/stage"
 
 ZLIB_INCLUDE="${stage}"/packages/include/zlib
 
-[ -f "$ZLIB_INCLUDE"/zlib.h ] || fail "You haven't installed the zlib package yet."
+[ -f "$ZLIB_INCLUDE"/zlib.h ] || fail "You haven't yet run 'autobuild install'."
 
 pushd "$FONTCONFIG_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
 
-        "linux")
+        linux*)
             # Linux build environment at Linden comes pre-polluted with stuff that can
             # seriously damage 3rd-party builds.  Environmental garbage you can expect
             # includes:
@@ -46,25 +55,23 @@ pushd "$FONTCONFIG_SOURCE_DIR"
             #
             # unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
 
-            # Prefer gcc-4.6 if available.
-            if [[ -x /usr/bin/gcc-4.6 && -x /usr/bin/g++-4.6 ]]; then
-                export CC=/usr/bin/gcc-4.6
-                export CXX=/usr/bin/g++-4.6
-            fi
+##          # Prefer gcc-4.6 if available.
+##          if [[ -x /usr/bin/gcc-4.6 && -x /usr/bin/g++-4.6 ]]; then
+##              export CC=/usr/bin/gcc-4.6
+##              export CXX=/usr/bin/g++-4.6
+##          fi
 
-            # Default target to 32-bit
-            opts="${TARGET_OPTS:--m32}"
+            # Default target per autobuild --address-size
+            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD}"
 
             # Handle any deliberate platform targeting
-            if [ -z "$TARGET_CPPFLAGS" ]; then
+            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
                 # Remove sysroot contamination from build environment
                 unset CPPFLAGS
             else
                 # Incorporate special pre-processing flags
                 export CPPFLAGS="$TARGET_CPPFLAGS" 
             fi
-
-            # First debug
 
             # Fontconfig is a strange one.  We use it in the Linux build and we ship it
             # but we ship the .so in a way that it isn't used in an installation.  Worse,
@@ -75,56 +82,34 @@ pushd "$FONTCONFIG_SOURCE_DIR"
             # A better fix is to build this right and use it or just remove it (and
             # freetype).
 
-            # Anyway, configure-time debug LDFLAGS references both debug and release
-            # as source packages may only have release.  --disable-silent-rules is
-            # present for chatty log files so you can review the actual library link
-            # and confirm it's sane.  Point configuration to use libexpat from
-            # dependent packages.  Make-time LDFLAGS adds an --exclude-libs option
-            # to prevent re-export of archive symbols.
+            # Anyway, --disable-silent-rules is present for chatty log files
+            # so you can review the actual library link and confirm it's sane.
+            # Point configuration to use libexpat from dependent packages.
+            # Make-time LDFLAGS adds an --exclude-libs option to prevent
+            # re-export of archive symbols.
 
-            CFLAGS="$opts -g -O0" \
-                CXXFLAGS="$opts -g -O0" \
-                LDFLAGS="$opts -g -L$stage/packages/lib/debug/ -L$stage/packages/lib/release/" \
-                ./configure \
-                --enable-static --enable-shared --disable-docs \
-                --with-pic --without-pkgconfigdir --disable-silent-rules \
-                --with-expat-includes="$stage"/packages/include/expat/ \
-                --with-expat-lib="$stage"/packages/lib/release/ \
-                --prefix="$stage" --libdir="$stage"/lib/debug/
-            make LDFLAGS="$opts -g -L$stage/packages/lib/debug/ -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
-            make install LDFLAGS="$opts -g -L$stage/packages/lib/debug/ -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
-
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                make check LDFLAGS="$opts -g -L$stage/packages/lib/debug/ -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
-            fi
-
-            make distclean 
-
-            # Release last
-            CFLAGS="$opts -g -O2" \
-                CXXFLAGS="$opts -g -O2" \
-                LDFLAGS="$opts -g -L$stage/packages/lib/release/" \
+            CFLAGS="$opts" \
+                CXXFLAGS="$opts" \
+                LDFLAGS="$opts -L$stage/packages/lib/release/" \
                 ./configure \
                 --enable-static --enable-shared --disable-docs \
                 --with-pic --without-pkgconfigdir --disable-silent-rules \
                 --with-expat-includes="$stage"/packages/include/expat/ \
                 --with-expat-lib="$stage"/packages/lib/release/ \
                 --prefix="$stage" --libdir="$stage"/lib/release/
-            make LDFLAGS="$opts -g -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
-            make install LDFLAGS="$opts -g -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
+            make LDFLAGS="$opts -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
+            make install LDFLAGS="$opts -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                make check LDFLAGS="$opts -g -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
+                make check LDFLAGS="$opts -L$stage/packages/lib/release/ -Wl,--exclude-libs,libz:libxml2:libexpat:libfreetype"
             fi
 
             make distclean 
         ;;
 
         *)
-            echo "build not supported."
-            exit -1
+            fail "Unsupported AUTOBUILD_PLATFORM='$AUTOBUILD_PLATFORM'."
         ;;
     esac
 
@@ -141,4 +126,3 @@ cp -a README.Linden "$stage"/docs/fontconfig/
 
 sed -n -E "s/PACKAGE_VERSION='([0-9.]+)'.*/\\1/p" "$FONTCONFIG_SOURCE_DIR/configure" > "$stage/VERSION.txt"
 pass
-
